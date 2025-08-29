3075753181bc03161bdacb01ddada5a3bda540c2
Tharuu003 <ar113029@fhss.sjp.ac.lk>
2025-08-29 13:46:37 +0530

    Invoice Update 1
    * The customer name input has been changed to a simple **textbox** for manual entry.
    * The date field now uses a **calendar** control for easier selection.
    * The payment method has been updated to a dropdown menu with three distinct options: **Card**, **Cash**, and **Bank Transfer**.

---
+++ SilkShield-New/SilkShield-New/ViewModel/NewInvoice1ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkShield_New.ViewModel
+{
+    internal class NewInvoice1ViewModel
+    {
+    }
+}
