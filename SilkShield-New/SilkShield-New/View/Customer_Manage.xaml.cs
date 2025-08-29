b3ab79be174d702140c905a6e9891e37f66a1935
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-08-29 13:00:08 +0530

    feat: Customer Management main window frontend
    This commit adds the core UI elements and layout for the customer management main window. It includes the following:
    Basic layout and structure
    Customer list table
    Search and filter controls
    Add New Customer' button

---
+++ SilkShield-New/SilkShield-New/View/Customer_Manage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SilkShield_New.View
+{
+    /// <summary>
+    /// Interaction logic for Customer_Manage.xaml
+    /// </summary>
+    public partial class Customer_Manage : Window
+    {
+        public Customer_Manage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+           
+
+        }
+    }
+}
