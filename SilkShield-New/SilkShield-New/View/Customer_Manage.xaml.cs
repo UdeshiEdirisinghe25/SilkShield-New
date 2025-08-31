623087b38d7884932b2f8fd3521ee87b44b294ae
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-08-31 20:46:53 +0530

    Create customer and databasehelper classr

--- SilkShield-New/SilkShield-New/View/Customer_Manage.xaml.cs
+++ SilkShield-New/SilkShield-New/View/Customer_Manage.xaml.cs
@@ -1,35 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SilkShield_New.View
 {
-    /// <summary>
-    /// Interaction logic for Customer_Manage.xaml
-    /// </summary>
     public partial class Customer_Manage : Window
     {
         public Customer_Manage()
         {
             InitializeComponent();
+            LoadCustomers();
+        }
+
+        private void LoadCustomers()
+        {
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            AddNewCustomer newCustomerWindow = new AddNewCustomer();
-            newCustomerWindow.Show();
+            // Open AddNewCustomer window
+            AddNewCustomer addWindow = new AddNewCustomer();
+            addWindow.ShowDialog();
 
-
+            // Refresh the DataGrid after adding a new customer
+            LoadCustomers();
         }
     }
 }
