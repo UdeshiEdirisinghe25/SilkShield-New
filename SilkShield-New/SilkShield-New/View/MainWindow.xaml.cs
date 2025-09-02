bc077fd96c383c86083eafd7b1806c142b91be34
imasha-rupasinghe <ar112883@fhss.sjp.ac.lk>
2025-09-03 02:22:01 +0530

    created top bar window button function

--- SilkShield-New/SilkShield-New/View/MainWindow.xaml.cs
+++ SilkShield-New/SilkShield-New/View/MainWindow.xaml.cs
@@ -79,6 +79,25 @@ namespace SilkShield_New.View
                 this.DragMove();
         }
 
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
 
 
 
