90c95e66b2cef0386f04d201b7e935954f1f5785
Tharuu003 <ar113029@fhss.sjp.ac.lk>
2025-09-02 23:47:36 +0530

    Invoice history window created

---
+++ SilkShield-New/SilkShield-New/View/InvoiceItem.cs
@@ -0,0 +1,11 @@
+﻿namespace SilkShield_New.View
+{
+    public class InvoiceItem
+    {
+        public string InvoiceID { get; set; }
+        public string Customer { get; set; }
+        public string Date { get; set; }
+        public double Amount { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ SilkShield-New/SilkShield-New/View/StatusToTextBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SilkShield_New.View
+{
+    public class StatusToTextBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string status)
+            {
+                if (status.Equals("Paid", StringComparison.OrdinalIgnoreCase))
+                {
+                    return new SolidColorBrush(Color.FromArgb(255, 0, 100, 0));
+                }
+                else if (status.Equals("Unpaid", StringComparison.OrdinalIgnoreCase))
+                {
+                    return new SolidColorBrush(Color.FromArgb(255, 139, 0, 0));
+                }
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
