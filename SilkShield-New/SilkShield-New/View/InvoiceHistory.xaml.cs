e348ab4fbd1b91b7b79f4cb7d48406bbf4b45480
Tharuu003 <ar113029@fhss.sjp.ac.lk>
2025-09-04 02:21:11 +0530

    Added clear options and date pickers.

--- SilkShield-New/SilkShield-New/View/InvoiceHistory.xaml.cs
+++ SilkShield-New/SilkShield-New/View/InvoiceHistory.xaml.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SilkShield_New.View
 {
-    /// <summary>
-    /// Interaction logic for InvoiceHistory.xaml
-    /// </summary>
     public partial class InvoiceHistory : Window
     {
         public InvoiceHistory()
         {
             InitializeComponent();
+        }
+
+        private void ClearSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchTextBox.Text = string.Empty;
+        }
+
+        // Common clear button for both From and To dates
+        private void ClearDates_Click(object sender, RoutedEventArgs e)
+        {
+            FromDatePicker.SelectedDate = null;
+            ToDatePicker.SelectedDate = null;
         }
     }
 }
