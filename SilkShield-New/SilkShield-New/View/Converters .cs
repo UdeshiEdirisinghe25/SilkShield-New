cb231572580b07bd9a8fb28089d0c0c1779792bc
Tharuu003 <ar113029@fhss.sjp.ac.lk>
2025-09-04 02:21:11 +0530

    Added clear options and date pickers.

---
+++ SilkShield-New/SilkShield-New/View/Converters .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SilkShield_New.View
+{
+    public class TextNotEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
