bdfc055bcb248cdb1dab7087a13e32fcb954a226
UdeshiEdirisinghe25 <ar112949@fhss.sjp.ac.lk>
2025-09-06 01:23:10 +0530

    Edited UI

---
+++ SilkShield-New/SilkShield-New/Model/InvoiceHistoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkShield_New.Model
+{
+    public class InvoiceHistoryModel
+    {
+        public int InvoiceId { get; set; }
+        public string Customer {  get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+
+    }
+}
---
+++ SilkShield-New/SilkShield-New/ViewModel/InvoiceHistoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using SilkShield_New.Data;
+using SilkShield_New.Model;
+
+
+
+namespace SilkShield_New.ViewModel
+{
+    
+    
+}
